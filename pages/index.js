4564431c0e27b37cc6073b9b250462ab6efdc6e8
GerardoQ <gerardoquispe65@gmail.com>
2020-11-14 15:30:48 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,97 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  FacebookShareCount,
+  HatenaShareButton,
+  InstapaperShareButton,
+  LineShareButton,
+  LinkedinShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  OKShareButton,
+  PinterestShareButton,
+  PocketShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TumblrShareButton,
+  TwitterShareButton,
+  ViberShareButton,
+  VKShareButton,
+  WhatsappShareButton,
+  WorkplaceShareButton
+} from "react-share";
+
+import {InlineShareButtons} from 'sharethis-reactjs';
 
 export default function Home() {
+
+  const shareUrl = "facebook.com"
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+     
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        hello :v
+        {/* <WhatsappShareButton title="a" url="facebook.com"></WhatsappShareButton>
+        <FacebookShareCount url={shareUrl}>
+        {shareCount => <span className="myShareCountWrapper">{shareCount}</span>}
+      </FacebookShareCount> */}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+<div>
+        <style dangerouslySetInnerHTML={{__html: `
+          html, body {
+            margin: 0;
+            padding: 0;
+            text-align: center;
+          }
+          h1 {
+            font-size: 24px;
+            font-weight: bold;
+          }
+          hr {
+            margin-bottom: 40px;
+            margin-top: 40px;
+            width: 50%;
+          }
+        `}} />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <h1>Compartir</h1>
+        <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'null',        // button labels (cta, counts, null)
+            language: 'es',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+              'whatsapp',
+              'facebook',
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            // show_total: true,
+            size: 40,             // the size of each button (INTEGER)
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            // OPTIONAL PARAMETERS
+            url: 'https://regalistos.pe', // (defaults to current url)
+            image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+            description: 'sadsadsadsad',       // (defaults to og:description or twitter:description)
+            title: 'custom title',            // (defaults to og:title or twitter:title)
+            message: 'custom email text',     // (only for email sharing)
+            subject: 'custom email subject',  // (only for email sharing)
+            username: 'custom twitter handle' // (only for twitter sharing)
+          }}
+        />
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </main>
     </div>
   )
 }
