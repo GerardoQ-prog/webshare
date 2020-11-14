7f367aa707c7b9b1023b70fa2657ca624f7e3bc1
GerardoQ <gerardoquispe65@gmail.com>
2020-11-14 15:52:33 -0500

    sadsad

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
         .then(() => console.log('Share was successful.'))
         .catch((error) => console.log('Sharing failed', error));
       } else {
-        console.log(`Your system doesn't support sharing files.`);
+        alert(`Your system doesn't support sharing files.`);
       }
 
 }
